995bf9e675771219ceab6a700ccd80f283c91b72
Ankit Balhara <144314774+BalharaAnkit@users.noreply.github.com>
2025-09-01 04:19:48 +0530

    Update urls.py

--- Login/urls.py
+++ Login/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.load_login, name="login"),  # /login/
-    path('customer/<int:id>/', views.load_customer, name="customer"),  # /login/customer/1/
-    path('waiter/<int:id>/', views.load_waiter),  # /login/waiter/1/
-    path('chef/<int:id>/', views.load_chef),  # /login/chef/1/
-    path('update-stock/<int:item_id>/<int:id>/<str:operation>/', views.update_stock, name='update_stock'),
-    path('update-order/<int:order_number>/<int:id>/<str:operation>/', views.update_order, name='update_order'),
-    path('update-order-waiter/<int:order_number>/<int:id>/<str:operation>/', views.update_order_waiter, name='update_order_waiter'),
+    path('', views.load_login, name="login"),   # http://127.0.0.1:8000/Login/
+    path('Customer/<int:id>/', views.load_customer, name="customer"),
+    path('Waiter/<int:id>/', views.load_waiter),
+    path('Chef/<int:id>/', views.load_chef),
+    path('update_stock/<int:item_id>/<int:id>/<str:operation>/', views.update_stock, name='update_stock'),
+    path('update_order/<int:order_number>/<int:id>/<str:operation>/', views.update_order, name='update_order'),
+    path('update_order_waiter/<int:order_number>/<int:id>/<str:operation>/', views.update_order_waiter, name='update_order_waiter'),
 ]
