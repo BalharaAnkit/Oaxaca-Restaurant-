f34842ce5fd282bbd1d3b8a6c2e8eb9ae69b071c
Ankit Balhara <144314774+BalharaAnkit@users.noreply.github.com>
2025-09-01 04:20:14 +0530

    Update urls.py

--- Register/urls.py
+++ Register/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.load_register, name="register"),  # /register/
+    path('', views.load_register, name="register"),   # http://127.0.0.1:8000/Register/
 ]
