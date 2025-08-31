070639dbc5197715126d68eae622faafc74173d6
Ankit Balhara <144314774+BalharaAnkit@users.noreply.github.com>
2025-09-01 04:03:56 +0530

    Update urls.py

--- Home/urls.py
+++ Home/urls.py
@@ -2,13 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('Home/', views.load_home, name="home"),
-    path('Menu/', views.load_menu, name="menu"),
-    path('About/', views.load_about, name="about"),
-    path('Contact/', views.load_contact, name="contact"),
-    path('Make-Order/', views.process_order, name='process-order'),
-    path('Home/booking.html', views.load_booking, name='booking'),
-    path('subscribe/', views.subscribe, name='subscribe'),
-
+    path('', views.load_home, name="home"),  # /home/
+    path('menu/', views.load_menu, name="menu"),  # /home/menu/
+    path('about/', views.load_about, name="about"),  # /home/about/
+    path('contact/', views.load_contact, name="contact"),  # /home/contact/
+    path('make-order/', views.process_order, name='process-order'),  # /home/make-order/
+    path('booking/', views.load_booking, name='booking'),  # /home/booking/
+    path('subscribe/', views.subscribe, name='subscribe'),  # /home/subscribe/
 ]
-
